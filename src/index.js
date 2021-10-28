230b9783e2199ac536c08682d2e3d14b8086d62b
firmnardians <firmnardians@gmail.com>
2021-10-28 22:46:13 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// Source : {
+// https://www.freecodecamp.org/news/javascript-modules-a-beginner-s-guide-783f7d7a5fcc/
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+// }
+
+// kodok is Promise HTTP Javascript
+
+const kodok = (function () {
+	const get = async function (url = '') {
+		const hit = await fetch(url, {
+			method: 'GET',
+		}).then((r) => r.json().then((d) => ({ status: r.status, data: d })));
+
+		return hit;
+	};
+
+	return {
+		get: get,
+	};
+})();
+
+export default kodok;
